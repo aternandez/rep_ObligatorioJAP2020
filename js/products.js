c39e77095a18535777948873d80338a80c2762e0
aternandez <68953737+aternandez@users.noreply.github.com>
2020-08-29 23:17:45 -0300

    final entrega 30aug2020

--- js/products.js
+++ js/products.js
@@ -4,7 +4,11 @@ const ORDER_ASC_BY_COST = "cost -> COST";
 const ORDER_DESC_BY_COST = "COST -> cost";
 const ORDER_DESC_BY_SOLDCOUNT = "SOLDCOUNT -> SoldCount";
 
+var minCost = undefined;
+var maxCost = undefined;
+
 var buscar = undefined;
+
 
 var carsArray = [];
 
@@ -41,14 +45,19 @@ function showCategoriesList(array) {
     for (let i = 0; i < array.length; i++) {
         let car = array[i];
 
-        if ((buscar == undefined || car.name.toLowerCase().indexOf(buscar) != -1) || 
-        (buscar == undefined || car.description.toLowerCase().indexOf(buscar) != -1)) {
+        if (((minCost == undefined) || (minCost != undefined && parseInt(car.cost) >= minCost)) &&
+            ((maxCost == undefined) || (maxCost != undefined && parseInt(car.cost) <= maxCost))) {
+
+
+
+            if ((buscar == undefined || car.name.toLowerCase().indexOf(buscar) != -1) ||
+                (buscar == undefined || car.description.toLowerCase().indexOf(buscar) != -1)) {
 
 
 
 
 
-            htmlContentToAppend += `
+                htmlContentToAppend += `
             <div class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
@@ -67,6 +76,7 @@ function showCategoriesList(array) {
                 </div>
             </div>
             `
+            }
         }
 
         document.getElementById("list-container").innerHTML = htmlContentToAppend;
@@ -86,43 +96,84 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showCategoriesList(carsArray);
         }
     });
+
+    // -- addEventListener para llamar funcion de filtro al clickear el botón de filtro --//
+    document.getElementById("filtrar").addEventListener("click", function () {
+
+        minCost = document.getElementById("rangeFilterCountMin").value;
+        maxCost = document.getElementById("rangeFilterCountMax").value;
+
+        if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0) {
+            minCost = parseInt(minCost);
+        }
+        else {
+            minCost = undefined;
+        }
+
+        if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0) {
+            maxCost = parseInt(maxCost);
+        }
+        else {
+            maxCost = undefined;
+        }
+
+        showCategoriesList(carsArray);
+    });
+
+    // -- addEventListener para llamar funcion de limpiar filtro al clickear el botón de limpiar filtro --//
+
+    document.getElementById("limpiar-filtro").addEventListener("click", function () {
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+
+        minCost = undefined;
+        maxCost = undefined;
+
+        showCategoriesList(carsArray);
+    });
+
+    //-- addEventListener para llamar funcion sort al clickear el botón de orden--//
+
+    document.getElementById("sortCostAsc").addEventListener("click", function () {
+        carsArray = sortCars(ORDER_ASC_BY_COST, carsArray);
+
+        showCategoriesList(carsArray);
+    });
+
+    document.getElementById("sortCostDesc").addEventListener("click", function () {
+        carsArray = sortCars(ORDER_DESC_BY_COST, carsArray);
+
+        showCategoriesList(carsArray);
+    });
+
+    document.getElementById("sortSoldCount").addEventListener("click", function () {
+        carsArray = sortCars(ORDER_DESC_BY_SOLDCOUNT, carsArray);
+
+        showCategoriesList(carsArray);
+    });
+
+    // addEventListener para llamar funcion buscar 
+    document.getElementById("buscador").addEventListener('input', function () {
+
+        buscar = document.getElementById("buscador").value.toLowerCase();
+
+        showCategoriesList(carsArray);
+
+    });
+
+    // addEventListener para limpiar barra de búsqueda 
+    document.getElementById("limpiar-busqueda").addEventListener("click", function () {
+        document.getElementById("buscador").value = "";
+
+        buscar = undefined;
+
+        showCategoriesList(carsArray);
+
+    });
+
 });
 
-//-- addEventListener para llamar funcion sort al clickear el botón de orden--//
 
-document.getElementById("sortCostAsc").addEventListener("click", function () {
-    carsArray = sortCars(ORDER_ASC_BY_COST, carsArray);
 
-    showCategoriesList(carsArray);
-});
 
-document.getElementById("sortCostDesc").addEventListener("click", function () {
-    carsArray = sortCars(ORDER_DESC_BY_COST, carsArray);
 
-    showCategoriesList(carsArray);
-});
-
-document.getElementById("sortSoldCount").addEventListener("click", function () {
-    carsArray = sortCars(ORDER_DESC_BY_SOLDCOUNT, carsArray);
-
-    showCategoriesList(carsArray);
-});
-
-// addEventListener para llamar funcion buscar 
-document.getElementById("buscador").addEventListener('input', function () {
-
-    buscar = document.getElementById("buscador").value.toLowerCase();
-
-    showCategoriesList(carsArray);
-
-});
-
-// addEventListener para limpiar barra de búsqueda <<FUNCIONANDO OK>>
-document.getElementById("limpiar").addEventListener("click", function () {
-    document.getElementById("buscador").value = "";
-
-    buscar = undefined;
-
-    showCategoriesList(carsArray);
-
-});
